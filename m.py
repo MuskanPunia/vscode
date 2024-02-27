928297ba4eb56b9e8e54ef7b4ab732d8805bdf2a
Muskan <muskanpunia143@gmail.com>
2024-02-27 10:19:35 +0530

    Good

---
+++ m.py
@@ -0,0 +1,14 @@
+a= int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+ope=input("Select the operator")
+if ope =='+':
+    print("The sum of a and b is",a+b)
+elif ope =='-':
+    print("The difference of a and b is",a-b)
+elif ope =='*':
+    print("The multiplication of a and b is",a*b)
+elif ope=='/':
+    print("the division of a and b is",a/b)
+else:
+    print("Invalid")
+
